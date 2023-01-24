d16f701ba083e7575dbe0b718a371fe94cdd7833
isaias <isaias@pop-os.localdomain>
2023-01-24 19:22:02 -0300

    inicio

--- meuapp/src/app/app.module.ts
+++ meuapp/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoursesModule } from './courses/courses.module';
+import { CoursesModule } from '../courses/courses.module';
 
 @Module({
   imports: [
